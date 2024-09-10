5c5325f5c45c9f4a61cfdfd24354d300a876cf78
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-09 21:28:42 -0300

    Atividade 2

---
+++ Atividades/atividade_2.py
@@ -0,0 +1,6 @@
+##2 - Imprima a frase: Meu nome é {nome} e tenho {idade} anos em que nome e idade precisam ser valores armazenados em variáveis.
+
+nome = "Guilherme"
+idade = 21
+
+print(f"Meu nome é {nome} e tenho {idade} anos")
