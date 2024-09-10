a1cd5e0cbdca68ef65b0a287cc38604ddf44f919
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-09 21:54:51 -0300

    Tipo int e bool

--- Sabor_express/app.py
+++ Sabor_express/app.py
@@ -13,9 +13,7 @@ print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input("Escolha uma opção: ")
-
-print(f"Você escolheu a opção {opcao_escolhida}!")
+opcao_escolhida = int(input("Escolha uma opção: "))
 
 if opcao_escolhida == 1:
     print("Cadastrar Restaurante")
