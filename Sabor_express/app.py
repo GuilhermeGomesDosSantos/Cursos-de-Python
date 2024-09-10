1dd3b1029a3c9cb7c27aac33c3bc6a54af826051
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-09 21:49:52 -0300

    If else

--- Sabor_express/app.py
+++ Sabor_express/app.py
@@ -16,3 +16,15 @@ print('4. Sair\n')
 opcao_escolhida = input("Escolha uma opção: ")
 
 print(f"Você escolheu a opção {opcao_escolhida}!")
+
+if opcao_escolhida == 1:
+    print("Cadastrar Restaurante")
+
+elif opcao_escolhida == 2:
+    print("Listar Restaurantes")
+
+elif opcao_escolhida == 3:
+    print("Ativar Restaurante")
+
+else:
+    print("Sair")
