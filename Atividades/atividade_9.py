7e44d0b67926b84d84027cadb227d4d9a159163e
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-11 11:09:26 -0300

    Atividade 9

---
+++ Atividades/atividade_9.py
@@ -0,0 +1,10 @@
+# Crie uma lista para cada informação a seguir:
+
+#  Lista de números de 1 a 10;
+#  Lista com quatro nomes;
+#  Lista com o ano que você nasceu e o ano atual.
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+nomes = [ 'Fulano', 'Fulana', 'Guilherme', 'GDS']
+datas = 2003, 2024
+
