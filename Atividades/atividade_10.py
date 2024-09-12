97f33b83424905cebfb2cb6fdc1a5f45940b0118
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-12 00:01:28 -0300

    Atividade 10

---
+++ Atividades/atividade_10.py
@@ -0,0 +1,8 @@
+#Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
+
+itens = ["ABC", 123, 15.6, "GDS"]
+
+print("Elementos da lista")
+
+for iten in itens:
+    print(f"- {iten}")
