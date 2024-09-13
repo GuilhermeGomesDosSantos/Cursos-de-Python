543453e85a5b35c373342d80e7c0978b953c9eb6
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-12 22:43:39 -0300

    Atividade 19

---
+++ Atividades/atividade_19.py
@@ -0,0 +1,9 @@
+# Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário.
+
+pessoa = {"nome": "Guilherme", "idade": 21, "cidade": "São Paulo"}
+
+if 'idade' in pessoa:
+    print("A chave 'idade' existe no dicionário")
+
+else:
+    print("A chave 'idade' não existe no dicionário")
