54a34a1e37c9272fbda38906aaaed48d9508754b
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-12 22:30:01 -0300

    Atividade 18

---
+++ Atividades/atividade_18.py
@@ -0,0 +1,8 @@
+#Crie um dicionário utilizando para representar números e seus quadrados de 1 a 5.
+
+num = [1, 2, 3, 4, 5]
+
+for n in num:
+    resultado = n*n
+
+    print(f"O valor ao quadrado de {n} é: {n * n}")
