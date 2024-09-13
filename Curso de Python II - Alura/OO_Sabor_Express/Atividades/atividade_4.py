719f6fb232e80d516dcd177b0a8b4f256ce6adcc
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-13 10:01:50 -0300

    Atividade 4

---
+++ Curso de Python II - Alura/OO_Sabor_Express/Atividades/atividade_4.py
@@ -0,0 +1,10 @@
+# Acesse o valor do atributo de classe categoria diretamente da classe Restaurante e armazene em uma variável chamada categoria.
+
+class Restaurante():
+    nome = ""
+    categoria = ""
+    ativo = False
+
+restaurante_praca = Restaurante()
+
+categoria = restaurante_praca.categoria
