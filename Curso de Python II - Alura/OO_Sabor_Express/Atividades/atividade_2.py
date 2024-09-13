f2bde4bc1cd78f3eb32f3e4371bcfa6156c7f983
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-13 09:41:54 -0300

    Atividade 2

---
+++ Curso de Python II - Alura/OO_Sabor_Express/Atividades/atividade_2.py
@@ -0,0 +1,10 @@
+# Acesse o valor do atributo nome da instância restaurante_praca da classe Restaurante.
+
+class Restaurante():
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+
+print(f"Nome: {restaurante_praca.nome}")
