b571bc9e75f44684fe403d7786f7c7fd1135a8f1
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-14 19:30:46 -0300

    Métodos especiais

--- Curso de Python II - Alura/OO_Sabor_Express/modelos/restaurante.py
+++ Curso de Python II - Alura/OO_Sabor_Express/modelos/restaurante.py
@@ -5,6 +5,11 @@ class Restaurante: # classe é uma abstração de um objeto do mundo em codigo
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+    
+    def __str__(self) -> str: #converte um valor qualquer em string
+        #lembrando novamente o self, é a referencia da instancia do obj que está sendo usado naquele momento
+        return f'{self.nome} | {self.categoria} | {self.ativo}'
+
 
 # instanciar um objeto/criando um restaurante
 restaurante_praca = Restaurante('Praça', 'Gourmet')
@@ -12,4 +17,5 @@ restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 restaurantes = [restaurante_pizza, restaurante_praca]
 
-print(restaurantes)
+print(restaurante_praca)
+print(restaurante_pizza)
