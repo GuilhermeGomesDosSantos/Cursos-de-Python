30dcbf1e1e1b32ea8212b29f529baf6b0c2693a6
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-14 11:58:00 -0300

    Construtor

--- Curso de Python II - Alura/OO_Sabor_Express/modelos/restaurante.py
+++ Curso de Python II - Alura/OO_Sabor_Express/modelos/restaurante.py
@@ -1,12 +1,14 @@
-class Restaurante(): # classe é uma abstração de um objeto do mundo em codigo
+class Restaurante: # classe é uma abstração de um objeto do mundo em codigo
+    def __init__(self, nome, categoria): # o def __init__ é um construtor que define quais parametros o objeto terá
+    # o self, refere-se á instância atual da classe, ele aponta para a instância específica
     # atributos, são as caracteristicas do objeto
-    nome = ''
-    categoria = ''
-    ativo = False
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
 # instanciar um objeto/criando um restaurante
-restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 restaurantes = [restaurante_pizza, restaurante_praca]
 
