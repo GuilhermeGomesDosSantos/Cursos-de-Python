31652b26865ac230d3db9332e27c612f84ee6728
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-14 09:34:36 -0300

    Atividade 7

---
+++ Curso de Python II - Alura/OO_Sabor_Express/Atividades/atividade_7.py
@@ -0,0 +1,13 @@
+# Imprima no console o nome e a categoria da instância restaurante_praca.
+
+class Restaurante():
+    nome = ""
+    categoria = ""
+    ativo = False
+
+restaurante_praca = Restaurante()
+
+restaurante_praca.nome = "Bistrô"
+restaurante_praca.categoria = "Italiana"
+
+print(f"Nome do restaurante '{restaurante_praca.nome}'\nCategoria '{restaurante_praca.categoria}'")
