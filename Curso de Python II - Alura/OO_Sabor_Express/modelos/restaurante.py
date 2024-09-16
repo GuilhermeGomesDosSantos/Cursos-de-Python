536cc26488c9a4a0703539a13d43a02e8971e4e2
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-09-16 20:01:15 -0300

    Property

--- Curso de Python II - Alura/OO_Sabor_Express/modelos/restaurante.py
+++ Curso de Python II - Alura/OO_Sabor_Express/modelos/restaurante.py
@@ -7,19 +7,25 @@ class Restaurante: # classe é uma abstração de um objeto do mundo em codigo
     # atributos, são as caracteristicas do objeto
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self) #Como restaurantes é um atributo da classe, você precisa acessar a lista usando a classe e não o self (que se refere à instância)
     
     def __str__(self) -> str: #converte um valor qualquer em string
         #lembrando novamente o self, é a referencia da instancia do obj que está sendo usado naquele momento
-        return f'{self.nome} | {self.categoria} | {self.ativo}'
+        return f'{self.nome} | {self.categoria}'
     
-    def listar_todos_restaurantes():# para criar sua propria função apenas utilize a palavra def
+    def listar_restaurantes():# para criar sua propria função apenas utilize a palavra def
+        print(f'{"Nome do Restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
 
+    @property #property em python é um decorartor, o property permite transformar um método(função) de uma classe em um atributo de leitura e vice versa
+    # permite modificar o comportamento de outra funlção ou método de forma simples e reutilizável
+    def ativo(self):
+        return f"☑" if self._ativo else "☒"
+    
 # instanciar um objeto/criando um restaurante
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-Restaurante.listar_todos_restaurantes()
+Restaurante.listar_restaurantes()
