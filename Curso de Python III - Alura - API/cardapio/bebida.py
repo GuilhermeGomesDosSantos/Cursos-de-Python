6c7837cebbcc94c8c27cdb79484775450579ac6d
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-10-09 10:44:06 -0300

    Herança

--- Curso de Python III - Alura - API/cardapio/bebida.py
+++
@@ -1,5 +0,0 @@
-class Bebida:
-    def __init__(self, nome, preco, tamanho):
-        self._nome = nome
-        self._preco = preco
-        self._tamaho = tamanho
--- Curso de Python III - Alura - API/cardapio/prato.py
+++
@@ -1,5 +0,0 @@
-class Prato:
-    def __init__(self, nome, preco, descricao):
-        self._nome = nome
-        self._preco = preco
-        self._descricao = descricao
