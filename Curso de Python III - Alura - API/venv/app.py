3b0c9862a28b32454d5bba8a53dd2487bf7956a7
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-10-16 13:45:54 -0300

    Filtrando dados

--- Curso de Python III - Alura - API/venv/app.py
+++ Curso de Python III - Alura - API/venv/app.py
@@ -7,7 +7,20 @@ response = requests.get(url)
 
 if response.status_code == 200:
     dados_json = response.json()
-    print(dados_json)
+    dados_restaurante = {}
 
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+
+        dados_restaurante[nome_do_restaurante].append({
+            "item": item['Item'],
+            "price": item['price'],
+            "description": item['description']
+        })
 else:
     print(f'Erro [{response.status_code}]')
+
+print(dados_restaurante['Pizza Hut'])
