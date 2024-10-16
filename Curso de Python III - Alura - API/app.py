c3205dae400b95e52d5fba44065e6ee05d78dfd2
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-10-15 22:51:57 -0300

    Polimorfismo

--- Curso de Python III - Alura - API/app.py
+++ Curso de Python III - Alura - API/app.py
@@ -4,14 +4,16 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante("praça", "gourmet")
 bebida_suco = Bebida("Suco de Melancia", 5.0, "grande")
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato("Pãozinho", 2.0, "O melhor pão da cidade")
+prato_paozinho.aplicar_desconto()
 
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 
 def main():
-    restaurante_praca.exibir_cardapio()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == "__main__":
     main()
